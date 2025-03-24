bc0f77997e324b7a7d3f6d9ad5271cd0b44ed588
Alexey Burmistrov <alexey_burmistrov@icloud.com>
2025-03-24 16:01:29 +0200

    Initial Commit

---
+++ UsersList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UsersList
+//
+//  Created by Alexey Burmistrov on 24.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UsersList/UsersListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersListApp.swift
+//  UsersList
+//
+//  Created by Alexey Burmistrov on 24.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct UsersListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
